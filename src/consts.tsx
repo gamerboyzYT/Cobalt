5ef855e1eedab26d7170ce9d3830c37cc7dadc40
gamerboyzYT <59750865+gamerboyzYT@users.noreply.github.com>
2023-10-10 22:00:34 -0700

    Update consts.tsx

--- src/consts.tsx
+++ src/consts.tsx
@@ -1,6 +1,6 @@
 const obfuscation = true;
 // @ts-ignore
-const bareServerURL = new URL("https://k12-fl.k12sciencemuseum.org/bare/");
+const bareServerURL = new URL("https://anura.mercurywork.shop/bare/");
 const github = "https://github.com/cognetwork-dev/Cobalt";
 const discord = "https://discord.com/invite/yk33HZSZkU";
 
